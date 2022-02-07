57f8124d59f9707aafaacdc13ace22b405f72a3d
Javier Avalos <javalos18j2@gmail.com>
2022-02-06 20:41:56 -0600

    Creacion de la funcion de mostrar la hora

---
+++ docs/js/app.js
@@ -0,0 +1,10 @@
+
+const mostrarReloj = () =>{
+    let fecha = new Date();
+    let hr = fecha.getHours();
+    let min = fecha.getMinutes();
+    let sec = fecha.getSeconds();
+
+    let hora = document.getElementById("hora");
+    hora.innerHTML = `${hr}:${min}:${sec}`
+}
