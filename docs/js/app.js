7ebae303eaeb7111ce3d8dfcaa534511261d1b1e
Javier Avalos <javalos18j2@gmail.com>
2022-02-06 20:58:47 -0600

    Agregamos la funcion para mostrar la fecha

--- docs/js/app.js
+++ docs/js/app.js
@@ -6,7 +6,17 @@ const mostrarReloj = () =>{
     let sec = formatoNum(fecha.getSeconds());
 
     let hora = document.getElementById("hora");
-    hora.innerHTML = `${hr}:${min}:${sec}`
+    hora.innerHTML = `${hr}:${min}:${sec}`;
+
+    const meses = ["Ene", "Feb", "Mar", "Abr", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"];
+    const dias = ["Lun", "Mar", "Mie", "Jue", "Vie", "Sab", "Dom"];
+
+    let diaSemana = dias[fecha.getDay()];
+    let mes = meses[fecha.getMonth()];
+
+    let fechaDiv = document.getElementById("fecha");
+    fechaDiv.innerHTML = `${diaSemana} - ${fecha.getDate()} - ${mes} - ${fecha.getFullYear()}`;
+
 }
 
 const formatoNum = (val) => val < 10 ? val = "0" + val : val = val;
